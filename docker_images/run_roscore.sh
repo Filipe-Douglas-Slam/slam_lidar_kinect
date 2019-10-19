8d572459a768014b12e125fbd1197350f3e7e7e3
Douglas Goulart <douglas.goul@gmail.com>
2019-10-19 18:18:08 -0300

    Created a Makefile to run all the scripts easily

--- docker_images/run_roscore.sh
+++
@@ -1,6 +0,0 @@
-docker run -it --rm \
-               --net foo \
-               --name master \
-               --env ROST_NAME=master \
-               ros:kinect \
-               roscore
--- docker_images/run_rviz.sh
+++
@@ -1,9 +0,0 @@
-docker run -it --rm \
-               -e DISPLAY \
-               -v /tmp/.X11-unix:/tmp/.X11-unix \
-               --net foo \
-               --name rviz \
-               --env ROS_HOSTNAME=rviz \
-               --env ROS_MASTER_URI=http://master:11311/ \
-               ros:kinect \
-	       rosrun rviz rviz
